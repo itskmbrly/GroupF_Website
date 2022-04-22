7f21a90170a64868afe4357bf070c6c156e9d400
Kimberly Edge <guzmankimberlyedgede@yahoo.com>
2022-04-22 18:00:21 +0800

    provided the bootstrap link, shortcut icon

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!--Shortcut Icon of Jentle Kare-->
+    <link rel="shortcut icon" href="images/JK.png">
+    <title>Jentle Kare</title>
+</head>
+<body>
+    
+</body>
+</html>
