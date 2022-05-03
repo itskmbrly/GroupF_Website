b49c29defa3740e6f91af195e766afbe40fc1141
Kimberly Edge <guzmankimberlyedgede@yahoo.com>
2022-05-03 13:03:13 +0800

    added a favorites.php, appointments.php, had some changes on the bootstrap, still working on the index.php

--- php/index.php
+++ php/index.php
@@ -24,8 +24,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap 5-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!--W3 School-->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -38,7 +38,7 @@
 <body>
     <!--SIDE NAVIGATION BAR-->
     <?php include_once("navbar.php"); ?>
-    <div style="margin-left:20%; background: #faf8e8;">
+    <div style="margin-left:20%; background-color: #faf8e8;">
         <!--ALERT MESSAGE-->
         <?php include_once("msg.php"); ?>
 
@@ -46,30 +46,34 @@
             <?php 
                 if($_SESSION["sess-role"] == 1){ //kraftsman
                     //REMOVE WHEN DONE: put here the welcome banner
-                    echo"<div>
-                            <h4>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
-                            <h2>Good Morning, $fname</h2>
-                            <h3>Welcome to JentleKare.</h3>
+                    echo"
+                        <div class='container p-5 my-5 bg-dark text-white'>
+                            <h4 style='text-align:center'>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
+                            <h2>Good Morning, $fname!</h2>
+                            <h5>Welcome to JentleKare.</h5>
                         </div>";
+                    echo"<h6>Book your appointments now!</h6>";
                 } else if($_SESSION["sess-role"] == 2){ //klient
-                    echo"<div>
-                            <h4>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
-                            <h2>Good Morning, $fname</h2>
-                            <h3>Welcome to JentleKare.</h3>
+                    echo"
+                        <div class='container p-5 my-5 border bg-white'>
+                            <h4 style='text-align:center'>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
+                            <h2>Good Morning, $fname!</h2>
+                            <h5>Welcome to JentleKare.</h5>
                         </div>";
-
+                    echo"<h6>Book your appointments now!</h6>";
                 } else if($_SESSION["sess-role"] == 3){ //admin
-                    echo"<div>
-                            <h4>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
-                            <h2>Good Morning, $fname</h2>
-                            <h3>Welcome to JentleKare.</h3>
+                    echo"
+                        <div class='container p-5 my-5 bg-dark text-white'>
+                            <h4 style='text-align:center'>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
+                            <h2>Good Morning, $fname!</h2>
+                            <h5>Welcome to JentleKare.</h5>
                         </div>";
-
                 } else if($_SESSION["sess-role"] == 4){ //moderator
-                    echo"<div>
-                            <h4>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
-                            <h2>Good Morning, $fname</h2>
-                            <h3>Welcome to JentleKare.</h3>
+                    echo"
+                        <div class='container p-5 my-5 bg-dark text-white'>
+                            <h4 style='text-align:center'>A PLATFORM FOR ALL YOUR BEAUTY NEEDS</h4>
+                            <h2>Good Morning, $fname!</h2>
+                            <h5>Welcome to JentleKare.</h5>
                         </div>";
 
                 }
