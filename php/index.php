e84fd923dfdab72ea4e8e6cfb12ffb08c63b9881
Kimberly Edge <guzmankimberlyedgede@yahoo.com>
2022-05-03 11:25:36 +0800

    updated the bootstrap 3 alert into 5

--- php/index.php
+++ php/index.php
@@ -23,10 +23,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- Bootstrap 5-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!--W3 School-->
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -39,7 +38,7 @@
 <body>
     <!--SIDE NAVIGATION BAR-->
     <?php include_once("navbar.php"); ?>
-    <div style="margin-left:20%">
+    <div style="margin-left:20%; background: #faf8e8;">
         <!--ALERT MESSAGE-->
         <?php include_once("msg.php"); ?>
 
--- php/msg.php
+++ php/msg.php
@@ -3,122 +3,123 @@
         if(isset($_GET["msg"])){
             if($_GET["msg"] == 1){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> First Name is Null.
                     </div>";
             } else if($_GET["msg"] == 2){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Last Name is null.
                     </div>";
             } else if($_GET["msg"] == 3){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Email is null.
                     </div>";
             } else if($_GET["msg"] == 4){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Password is null.
                     </div>";
             } else if($_GET["msg"] == 5){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Confirm Password is null.
                     </div>";
             } else if($_GET["msg"] == 6){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Mobile Number is null.
                     </div>";
             } else if($_GET["msg"] == 7){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Address is null.
                     </div>";
             } else if($_GET["msg"] == 8){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Barangay is null.
                     </div>";
             } else if($_GET["msg"] == 9){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> City is null.
                     </div>";
             } else if($_GET["msg"] == 10){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Province is null.
                     </div>";
             } else if($_GET["msg"] == 11){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Zip Code is null.
                     </div>";
             } else if($_GET["msg"] == 12){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Country is null.
                     </div>";
             } else if($_GET["msg"] == 13){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Role is null.
                     </div>";
             } else if($_GET["msg"] == 14){
                 echo"
-                    <div class='alert alert-success'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-success alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Login Successfully!</strong>
                     </div>";
             } else if($_GET["msg"] == 15){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        
                         <strong>Error!</strong> Invalid Password.
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                     </div>";
             } else if($_GET["msg"] == 16){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Email does not exists.
                     </div>";
             } else if($_GET["msg"] == 17){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> It looks like you've missed something. Please try again.
                     </div>";
             } else if($_GET["msg"] == 18){
                 echo"
-                    <div class='alert alert-success'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-success alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Welcome to Jentle Kare!</strong> You are successfully registered.
                     </div>";
             } else if($_GET["msg"] == 19){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Error!</strong> Password does not matched.
                     </div>";
             } else if($_GET["msg"] == 20){
                 echo"
-                    <div class='alert alert-danger'>
-                        <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                    <div class='alert alert-danger alert-dismissiblealert-dismissible'>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert'></button>
                         <strong>Duplicate Record!</strong> This user already exists.
                     </div>";
             } 
